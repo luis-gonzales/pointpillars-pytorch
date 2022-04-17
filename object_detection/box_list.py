77534ab1a33911e9687cd8937bf6b705fd0c3b29
Luis R. Gonzales <luis@kerb.ai>
2022-04-16 18:09:03 -0700

    fix boxes.shape conditional

--- object_detection/box_list.py
+++ object_detection/box_list.py
@@ -50,8 +50,7 @@ class BoxList(object):
         Raises:
             ValueError: if invalid dimensions for bbox data or if bbox data is not in float32 format.
         """
-        # if len(boxes.shape) != 2 or (boxes.shape[-1] != 6 and boxes.shape[-1] != 4):
-        if len(boxes.shape) != 2 or boxes.shape[-1] != 6:
+        if len(boxes.shape) != 2 or (boxes.shape[-1] != 6 and boxes.shape[-1] != 4):
             raise ValueError('Invalid dimensions for box data.')
         if boxes.dtype != torch.float32:
             raise ValueError('Invalid tensor type: should be tf.float32')
